2d6014de3db241f591e4bf864a8b8a89469de0da
Ariel Soto <arielestebansoto@gmail.com>
2021-06-16 16:36:17 -0300

    toggle Modal

--- js/toggleModal.js
+++ js/toggleModal.js
@@ -1,9 +1,14 @@
-const card = document.querySelector('.card-project')
-const modal = document.querySelector('.modal')
-card.addEventListener('click', toggleModal)
-
-function toggleModal() {
-    console.log(`funcara?`)
-    modal.style.display = 'block'
+const card = document.querySelectorAll('.card')
+card.forEach(element => {
+    element.addEventListener('click', toggleModal)
+})
+function toggleModal(element) {
+    element.stopPropagation()
+    let target = element.currentTarget
+    let modal = target.childNodes[1]
+    modal.style.display = "flex"
+    // let target = element.currentTarget
+    // let modal = target.childNode
+    // console.log(modal)
 }
 export default toggleModal
