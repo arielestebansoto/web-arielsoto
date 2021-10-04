de3039d1149f63d0b21c1fdee524c5dee7103749
Ariel Esteban Soto <arielestebansoto@gmail.com>
2021-10-04 16:30:58 -0300

    Basic Hero

---
+++ src/components/Hero/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { HeroContainer, Title, SubTitle } from './style'
+
+export const Hero = () => 
+    <HeroContainer> 
+        <Title>
+            <span>Hi,</span>
+            <br/>
+            <span>I'm Ariel,</span>
+            <br/>
+            <span>web developer.</span>
+        </Title>
+        <SubTitle>
+            React developer
+        </SubTitle>
+    </HeroContainer >
---
+++ src/components/Hero/style.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+
+export const HeroContainer = styled.div`
+    position: absolute;
+    top: 30%;
+    overflow:hidden;
+`
+export const Title = styled.h1`
+    font-size: 40px;
+    
+    @media screen and (min-width: 576px) {
+        font-size: 50px;
+    }
+    animation: showup 3s ease-in;
+
+    @keyframes showup {
+        0% { opacity: 0};
+        100% { opacity: 1};
+    }
+`
+
+export const SubTitle = styled.p`
+    animation: slide 2.5s ease-in;
+
+    @keyframes slide {
+        0% { opacity: 0; margin-top: -4px};
+        85% { opacity: 0; margin-top: -4px};
+        100% { opacity: 1; margin-top: 0px}
+    }
+`
