b3ab61b423c64b5f992c4b5dabdea0eca740ba4a
ariel <arielestebansoto@gmail.com>
2021-10-09 10:14:29 -0300

    animation rendering in dom

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import { SectionContainer, TitleContainer, Title, SkillsCharset, SkillItem } from './styles'
+import { animationElement, animate } from './animation'
 
 export const Skills = () => {
     const skills = [ 'React', 'React-Router', 'Redux', 'Bootstrap', 'Styled-Components', 'Git Hub', 'Webpack', 'Material UI', 'NodeJS', 'Linux', ,
     ]
+
+    useEffect( () => {
+        document.querySelector('#animationContainer').appendChild(animationElement)
+        animate()
+    })
+    
     return (
-        <SectionContainer>
+        <SectionContainer id='animationContainer'>
             <TitleContainer>
                 <Title>skills</Title>
             </TitleContainer>
