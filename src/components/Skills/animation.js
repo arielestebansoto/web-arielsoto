83d22622a61c1ed198efd1818df48dad7da1c158
Ariel Esteban Soto <arielestebansoto@gmail.com>
2021-10-12 17:17:01 -0300

    learing

--- src/components/Skills/animation.js
+++ src/components/Skills/animation.js
@@ -1,23 +1,49 @@
-import { Scene, PerspectiveCamera, WebGL1Renderer, Color, Mesh, BoxGeometry, MeshBasicMaterial } from "three";
+import { Scene, PerspectiveCamera, WebGL1Renderer, Color, Mesh, BoxGeometry, MeshBasicMaterial, Fog, TextureLoader, CameraHelper, OrthographicCamera } from "three";
 
 const scene = new Scene()
-scene.background = new Color('#0ff')
-const camera = new PerspectiveCamera(75, window.innerWidth/window.innerHeight)
+// scene.background = new Color(0x2a3b4c)
+// scene.fog = new Fog(0xff00ff, 0.1, 8)
+// const loader = new TextureLoader()
+
+const camera = new PerspectiveCamera(
+    75, 
+    window.innerWidth/window.innerHeight,
+    0.1,
+    2000
+    )
+const newCamera = new OrthographicCamera(
+    5,
+    -5,
+    5,
+    -5,
+    3,
+    10
+    )
+const helper = new CameraHelper(newCamera)
+scene.add(helper)
+
 const renderer = new WebGL1Renderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 
 const geometry = new BoxGeometry()
 const material = new MeshBasicMaterial({color: 'green', wireframe: true})
 const cube = new Mesh(geometry, material)
-
+cube.position.z = -3.2
 scene.add(cube)
 
-camera.position.z = 5
+// camera.position.z = 3.2
 
+let i = 0
 export const animate = () => {
     requestAnimationFrame(animate)
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
+
+    camera.lookAt(newCamera.position)
+    camera.position.x = Math.cos(i) * 30
+    camera.position.z = Math.sin(i) * 30
+
+    i += 0.01
+    // cube.rotation.x += 0.01
+    // cube.rotation.y += 0.01
     
     renderer.render(scene, camera)
 }
