a10d8202010ff0f71e345ffc1a3bbfa814e60664
Ariel Esteban Soto <arielestebansoto@gmail.com>
2021-10-13 16:30:30 -0300

    transform controls

--- src/components/Skills/animation.js
+++ src/components/Skills/animation.js
@@ -1,89 +1,73 @@
-import { Scene, PerspectiveCamera, WebGL1Renderer, Color, Mesh, BoxGeometry, MeshBasicMaterial, Fog, TextureLoader, CameraHelper, OrthographicCamera, CircleGeometry, ConeGeometry, CylinderGeometry, SphereGeometry } from "three";
+import { Scene, PerspectiveCamera, WebGL1Renderer, Color, Mesh, BoxGeometry, MeshBasicMaterial, Fog, TextureLoader, CameraHelper, OrthographicCamera, CircleGeometry, ConeGeometry, CylinderGeometry, SphereGeometry, GridHelper } from "three";
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls' 
+import { TransformControls } from 'three/examples/jsm/controls/TransformControls'
+import { DragControls } from 'three/examples/jsm/controls/DragControls'
+import { image } from "./image";
 
 const scene = new Scene()
-// scene.background = new Color(0x2a3b4c)
-// scene.fog = new Fog(0xff00ff, 0.1, 8)
-// const loader = new TextureLoader()
-
 const camera = new PerspectiveCamera(
     75, 
     window.innerWidth/window.innerHeight,
     0.1,
     2000
     )
-// const newCamera = new OrthographicCamera( 5, -5, 5, -5, 3, 10 )
-// const helper = new CameraHelper(newCamera)
-// scene.add(helper)
-
+    
 const renderer = new WebGL1Renderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
-
-const geometry = new BoxGeometry(2, 2, 2, 3, 4, 5)
-const material = new MeshBasicMaterial({color: 'green', wireframe: true})
-const cube = new Mesh(geometry, material)
-scene.add(cube)
-
-const circle = new Mesh( new CircleGeometry(2,32, 0, Math.PI), material)
-scene.add(circle)
-circle.position.x = 5
-
-const cone = new Mesh( new ConeGeometry(1, 3, 32, 5, true, 0, Math.PI ), material)
-scene.add(cone)
-cone.position.x = -5
-
-const cilinder = new Mesh( new CylinderGeometry(2, .5), material)
-scene.add(cilinder)
-cilinder.position.y = 5
-
-const sphere = new Mesh( new SphereGeometry( 1, 32, 32, 0, Math.PI, 0, Math.PI / 2), material)
-scene.add(sphere)
-sphere.position.y = -5
-
-camera.position.z = 10
-
-const controls = new OrbitControls(camera, renderer.domElement)
-
-controls.minDistance = 3
-controls.maxDistance = 10
-// controls.enableZoom = false
-// controls.enableRotate = false
-
-controls.enableDamping = true
-controls.dampingFactor = .5
-// controls.maxPolarAngle = Math.PI / 10
-
-controls.screenSpacePanning = true
-
-// let i = 0
-
 window.addEventListener('resize', redimencionar) 
+    
+const geometry = new BoxGeometry(2, 2, 2, 3, 4, 5)
+const material = new MeshBasicMaterial(
+    // {color: 'green', wireframe: true}
+    )
+    const cube = new Mesh(geometry, material)
+    scene.add(cube)
+    scene.add(new GridHelper(10, 20))
+    
+    camera.position.set(1, 1, 5)
+    // camera.position.z = 10
+    
+    const oControl = new OrbitControls(camera, renderer.domElement)
+    const tControl = new TransformControls(camera, renderer.domElement)
+    
+    tControl.addEventListener('mouseDown', e => {
+        oControl.enabled = false
+    })
+    tControl.addEventListener('mouseUp', e => {
+        oControl.enabled = true
+    })
 
-function redimencionar() {
-    console.log('redimencionando')
+    tControl.attach(cube)
+    scene.add(tControl)
+    tControl.setMode('translate')
+    // tControl.showX = false
+    // tControl.showY = false
+    // tControl.showZ = false
+    // tControl.setMode('rotate')
+    // tControl.setMode('scale')
+
+    const dContrl = new DragControls(camera, domElement)
+    
+    
+    function redimencionar() {
     camera.aspect = window.innerWidth/window.innerHeight
     camera.updateProjectionMatrix()
     renderer.setSize(window.innerWidth, window.innerHeight)
     renderer.render(scene, camera)
 }
 
+const loader = new TextureLoader()
+loader.load(image, 
+(texture) => material.map = texture )
+
 export const animate = () => {
     requestAnimationFrame(animate)
-
     scene.traverse( object => {
         if (object.isMesh) {
-            object.rotation.x += 0.01
-            object.rotation.y += 0.01
+            // object.rotation.x += 0.01
+            // object.rotation.y += 0.01
         }
     })
-    // camera.lookAt(newCamera.position)
-    // camera.position.x = Math.cos(i) * 30
-    // camera.position.z = Math.sin(i) * 30
-
-    // i += 0.01
-    // cube.rotation.x += 0.01
-    // cube.rotation.y += 0.01
-    
     renderer.render(scene, camera)
 }
 
