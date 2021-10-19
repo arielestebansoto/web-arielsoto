913c8a6b8694a8f1b316dbc7b44018e2d8cda023
ariel <arielestebansoto@gmail.com>
2021-10-19 08:54:28 -0300

    FXING THE FUCKING TEXT!!!!!!!!! I AM HAPPY

--- src/components/Animation/objects/texts/skills/index.js
+++ src/components/Animation/objects/texts/skills/index.js
@@ -2,13 +2,13 @@ import { Mesh, MeshPhongMaterial } from 'three'
 import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry'
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader'
 import { scene } from '../../../scene'
-import helvertiker from 'three/examples/fonts/helvetiker_regular.typeface.json'
+// import helvertiker from 'three/examples/fonts/helvetiker_regular.typeface.json'
 
 const loader = new FontLoader()
 
 export const textSkills = () => {
 
-    loader.load(helvertiker, function ( font ) {
+    loader.load('https://dl.dropboxusercontent.com/s/wjnohcz26odo5ak/Azonix_Regular.json?dl=0', function ( font ) {
 
             const text = new TextGeometry(
                 'skills',  
