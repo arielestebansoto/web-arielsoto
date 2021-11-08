8494d1230b14d6d509d91e6a1695b00c6237a535
Ariel Esteban Soto <arielestebansoto@gmail.com>
2021-11-08 18:32:18 -0300

    adding texts

--- src/components/Animation/objects/texts/texts.js
+++ src/components/Animation/objects/texts/texts.js
@@ -1,18 +1,67 @@
+const angleFront = Math.PI * 2
+const angleBack = Math.PI
+const angleRight = Math.PI * 1.5  
+const angleLeft = Math.PI * 0.5
+
 export const texts = [ 
     {
         title: 'skills', 
         position: { x: 0, y: 0, z: -1 },
-        rotation: { x: 0, y: 0, z: 0 },
+        rotation: { x: 0, y: angleFront, z: 0 },
     }, 
     {
         title: 'react', 
-        position: { x:  -10, y: 0, z: -4 },
-        rotation: { x: 0, y: 0, z: 0},
+        position: { x: 0, y: 0, z: -4 },
+        rotation: { x: 0, y: angleFront, z: 0},
     },
     {
         title: 'redux', 
-        position: { x:  -10, y: 0, z: -10 },
-        rotation: { x: 0, y: 0, z: 0 },
+        position: { x: 0, y: 0, z: -8 },
+        rotation: { x: 0, y: angleFront, z: 0 },
+    },
+    {
+        title: 'react-router', 
+        position: { x: 0, y: 0, z: -12 },
+        rotation: { x: 0, y: angleFront, z: 0 },
+    },
+    {
+        title: 'github', 
+        position: { x: 0, y: 0, z: -14},
+        rotation: { x: 0, y: angleFront, z: 0 },
+    },
+    {
+        title: 'nodejs', 
+        position: { x: 0, y: 0, z: 10},
+        rotation: { x: 0, y: angleBack, z: 0 }
+    },
+    {
+        title: 'styled-components', 
+        position: { x: -10, y: 0, z: -5 },
+        rotation: { x: 0, y: angleLeft, z: 0 },
+    },
+    {
+        title: 'materialize', 
+        position: { x:  -15, y: 0, z: 0 },
+        rotation: { x: 0, y: angleLeft, z: 0 },
+    },
+    {
+        title: 'bootsrap', 
+        position: { x: -20, y: 0, z: 5},
+        rotation: { x: 0, y: angleLeft, z: 0 },
+    },
+    {
+        title: 'sass', 
+        position: { x: -10, y: 0, z: 5 },
+        rotation: { x: 0, y: angleLeft, z: 0 },
+    },
+    {
+        title: 'html css js', 
+        position: { x:  -20, y: 0, z: -20 },
+        rotation: { x: 0, y: angleLeft, z: 0 },
+    },
+    {
+        title: 'threeJS', 
+        position: { x:  20, y: 0, z: 0 },
+        rotation: { x: 0, y: angleRight, z: 0 },
     }
-
 ]
