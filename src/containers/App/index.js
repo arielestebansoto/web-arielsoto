e1bf6cb26a3bfa19a297d3d5c1dd639ee5768c48
Ariel Esteban Soto <arielestebansoto@gmail.com>
2021-11-16 18:11:00 -0300

    global styles

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
 import { Home } from '../../pages/Home'
+import { GlobalStyle } from '../../globalStyles'
 
 export const App = () => {
     return (
         <>
+            <GlobalStyle />
             <Home />
         </>
     )
--- src/globalStyles/index.js
+++ src/globalStyles/index.js
@@ -1,7 +1,43 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        cursor: none;
+        margin: 0;
+        box-sizing: border-box;
+        font-weight: 100;
+    } 
+    body {
+        font-family: 'Exo 2', sans-serif;
+        overflow-x: hidden;
+        color: white;
+        min-height: 100vh;
+        background: #0f2027;
+        background: -webkit-linear-gradient(to bottom, #0f2027, #203a43, #2c5364);
+        background: linear-gradient(to bottom, #0f2027, #203a43, #2c5364); 
+    }
+
+    ::-webkit-scrollbar {
+        width: 6px;
+    }
+    ::-webkit-scrollbar-thumb {
+        background-color: rgba(133, 206, 248, 0);
+        border: 1px solid rgba(133, 206, 248, .5);
+        border-radius: 4px;
+        backdrop-filter: blur(7px);
+    }
+    ::-webkit-scrollbar-thumb:hover {
+        background-color: rgba(133, 206, 248, 0.2);
+    }
+    ::-webkit-scrollbar-track {
+        background: #0f2027;
+        background: -webkit-linear-gradient(to bottom, #0f2027, #203a43, #2c5364);
+        background: linear-gradient(to bottom, #0f2027, #203a43, #2c5364); 
+    }
+`
 export const sectionMarginBottom= 50
 export const titleMarginBottom = 32
 export const iconSize = 48 
-
 export const worksLiMarginBottom = 30
 export const worksItemFontSizeMobile = 30
 export const worksItemFontSizeDesktop = 50
