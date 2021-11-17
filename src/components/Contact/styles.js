b04eb071e715ad175dac1b846af7037d236ec1af
Ariel Esteban Soto <arielestebansoto@gmail.com>
2021-11-17 18:42:26 -0300

    pulse animation in social icons

--- src/components/Contact/styles.js
+++ src/components/Contact/styles.js
@@ -46,6 +46,8 @@ export const Ul = styled.ul`
     }
 `
 export const Li = styled.li`
+    position: relative;
+
     display: flex;
     justify-content: center;
     align-items: center;
@@ -59,8 +61,50 @@ export const Li = styled.li`
     &:hover {
         opacity: 1;
     }
+
+    &:after {
+        content: '';
+        position: absolute;
+        top: 100%;
+        background: rgba(0, 0, 0, .5);
+        width: 50px;
+        height: 10px;
+        filter: blur(3px);
+        border-radius: 50%;
+    }
+
+    &:before {
+        content: '';
+        position: absolute;
+        width: 35px;
+        height: 35px;
+        border-radius: 50%;
+        box-shadow: 0 0 10px 0 rgba(255, 255, 255, 1);
+        transform: scale(1);
+        animation: pulse-black 2s infinite;
+        
+        @keyframes pulse-black {
+            0% {
+              transform: scale(0.95);
+              box-shadow: 0 0 10px 0 rgba(255, 255, 255, 0.7);
+            }
+            
+            70% {
+              transform: scale(1);
+              box-shadow: 0 0 10px 20px rgba(255, 255, 255, 0);
+            }
+            
+            100% {
+              transform: scale(0.95);
+              box-shadow: 0 0 0 10px rgba(255, 255, 255, 0);
+            }
+          }
+    }
+    
 `
 export const Anchor = styled.a`
+    position: relative;
+
     display: flex;
     justify-content: center;
     align-items: center;
