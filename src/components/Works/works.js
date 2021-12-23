a0cf9b4ad056a7f436f080c3eab3583ab0f266ac
Ariel Esteban Soto <72299420+arielestebansoto@users.noreply.github.com>
2021-12-22 22:29:55 -0300

    Ecommerce project added and updating the projects.

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -1,14 +1,20 @@
 export const works = [
+     {
+        title: 'Ecommerce',
+        href: 'https://arielestebansoto.github.io/ecommerce/',
+        description: "A ecommerce website. Made with React. State managment with Redux. Routing with React-router. Material Design as css framework. For now is only avaible the front end. But I'm developing the backend."
+    },
     {
         title: 'Mercado Libre Home',
         href: 'https://arielestebansoto.github.io/mercado-libre-home/',
-        description: 'Made only with HTML and CSS.'
+        description: 'The UI of one of the best Ecommerce. Made only with HTML and CSS vanilla.'
     },
     {
         title: 'Breaking Bad API',
         href: 'https://arielestebansoto.github.io/breaking-bad-api/',
-        description: 'A simply project to fetch a API'
+        description: "A simply project to fetch a API."
     },
+   
     // {
     //     title: '',
     //     href: '',
